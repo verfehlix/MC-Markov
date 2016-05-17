5b04a39f6fdad9780f94da771835a919701e0dfd
Pascal Seitz <pascal.seitz@sap.com>
2016-05-17 09:54:53 +0200

    added hitler, moved usage to markover

---
+++ markover.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var markov = require('./modelBuilder_order1');
+
+var order = 2;
+
+markov.addToModel(fs.readFileSync("lyrics/trump.txt", 'utf8').toString(), order);
+
+markov.addToModel(fs.readFileSync("lyrics/hitler.txt", 'utf8').toString(), order);
+
+var resultText = markov.getMarkovText(100);
+console.log(resultText);
--- modelBuilder_order1.js
+++ modelBuilder_order1.js
@@ -1,90 +1,110 @@
 var State = require('./mcMarkov').State;
 var MarkovChain = require('./mcMarkov').MarkovChain;
 
-var fs = require('fs');
-
-var fileName = "lyrics/trump.txt";
-var text = fs.readFileSync(fileName, 'utf8').toString();
-// var text = "I am not a number! I am a free man!";
-
-var order = 2;
-var amountOfWords = 100;
-
-//split text into words
-
-text = text.replace(/(\r\n|\n|\r)/gm," ");
-text = text.replace(/\s\s+/g, ' ');
-
-var textArray = text.split(" ");
-var wordCount = textArray.length - 1;
+var service = {};
 
 var model = {};
 
-for (var i = 0; i < textArray.length - order; i++) {
-	var word = "";
-	
-	for (var j = i; j < i + order ; j++) {
-		word += textArray[j] + " ";
+
+function addToModel(text, order){
+	//split text into words
+	text = text.replace(/(\r\n|\n|\r)/gm," ");
+	text = text.replace(/\s\s+/g, ' ');
+
+	var textArray = text.split(" ");
+	var wordCount = textArray.length - 1;
+
+	console.log("Adding " + wordCount + " words");
+
+	for (var i = 0; i < textArray.length - order; i++) {
+		var word = "";
+		
+		for (var j = i; j < i + order ; j++) {
+			word += textArray[j] + " ";
+		}
+		word = word.trim();
+
+		var nachfolger = textArray[i+order];
+
+		if(!model[word]){ 
+			model[word] = {};
+		}
+
+		if(model[word][nachfolger]){
+			model[word][nachfolger]++;
+		} else {
+			model[word][nachfolger] = 1;
+		}	
+	};
+	// console.log(JSON.stringify(model));
+}
+
+var alreadyCalculatedProbabilities = false;
+function calcProbabilities(){
+	if (alreadyCalculatedProbabilities) {
+		console.log("alreadyCalculatedProbabilities");
+		return;
 	}
+	for (var wordKey in model) {
+		var word = model[wordKey];
 
-	word = word.trim();
+		var sum = 0;
+		for (var nachfolgerKey1 in word){
+			// var nachfolger = word[nachfolgerKey];
+			sum += word[nachfolgerKey1];
+		}
 
-	var nachfolger = textArray[i+order];
+		for (var nachfolgerKey2 in word){
+			word[nachfolgerKey2] = word[nachfolgerKey2] / sum;
+		}
+	};
+	alreadyCalculatedProbabilities = true;
+}
 
-	if(!model[word]){ 
-		model[word] = {};
-	}
+function createStates(){
+	var states = [];
 
-	if(model[word][nachfolger]){
-		model[word][nachfolger]++;
-	} else {
-		model[word][nachfolger] = 1;
-	}	
-};
+	for (var wordKey in model) {
+		var word = model[wordKey];
 
-for (var wordKey in model) {
-	var word = model[wordKey];
+		var nachfolgeStates = [];
+		var nachfolgeStatePropabilites = [];
+		for (var nachfolgerKey in word){
+			nachfolgeStates.push(nachfolgerKey);
+			nachfolgeStatePropabilites.push(word[nachfolgerKey]);
+		}
 
-	var sum = 0;
-	for (var nachfolgerKey1 in word){
-		// var nachfolger = word[nachfolgerKey];
-		sum += word[nachfolgerKey1];
-	}
+		var state = new State(wordKey, nachfolgeStates, nachfolgeStatePropabilites); 
 
-	for (var nachfolgerKey2 in word){
-		word[nachfolgerKey2] = word[nachfolgerKey2] / sum;
-	}
-};
+		states.push(state);
+	};
+	return states;
 
-// console.log(JSON.stringify(model));
-
-var states = [];
-
-for (var wordKey in model) {
-	var word = model[wordKey];
-
-	var nachfolgeStates = [];
-	var nachfolgeStatePropabilites = [];
-	for (var nachfolgerKey in word){
-		nachfolgeStates.push(nachfolgerKey);
-		nachfolgeStatePropabilites.push(word[nachfolgerKey]);
-	}
-
-	var state = new State(wordKey, nachfolgeStates, nachfolgeStatePropabilites); 
-
-	states.push(state);
-};
-
-var randomModelWort = Object.keys(model)[Math.floor(Math.random() * Object.keys(model).length - 1)  ];
-
-var markovChain = new MarkovChain(states, randomModelWort);
-// var markovChain = new MarkovChain(states, 'useless."');
+}
 
 
-var resultText = "";
-for (var i = 0; i < amountOfWords; i++) {
-	resultText += markovChain.printText + " ";
-	markovChain.goToNextState();
-};
+function getMarkovText(amountOfWords, startWord){
+	if(!startWord) startWord = Object.keys(model)[Math.floor(Math.random() * Object.keys(model).length - 1)  ];
 
-console.log(resultText);
+	calcProbabilities();
+	var states = createStates();
+	var markovChain = new MarkovChain(states, startWord);
+
+	var resultText = "";
+	for (var i = 0; i < amountOfWords; i++) {
+		resultText += markovChain.printText + " ";
+		markovChain.goToNextState();
+	};
+
+	return resultText;
+
+}
+
+
+
+
+service.addToModel = addToModel;
+service.calcProbabilities = calcProbabilities;
+service.getMarkovText = getMarkovText;
+
+module.exports = service;
